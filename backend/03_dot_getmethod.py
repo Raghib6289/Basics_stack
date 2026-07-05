89011f9f51bef10c6af8077f963c2058bacbc2fe
Md Raghib <kraghib123@gmail.com>
2026-07-05 23:21:50 +0530

    Enhance get method with default value for city
    Added default value for missing keys in user_profile.

--- backend/03_dot_getmethod.py
+++ backend/03_dot_getmethod.py
@@ -4,6 +4,7 @@ user_profile = {
     "age": 30
 }
 
+
 print(user_profile.get("username")) # Output: johndoe
 print(user_profile.get("city"))      # Output: None (key not found, returns None by default)
 print(user_profile.get("city", "Unknown")) # Output: Unknown (key not found, returns specified default value)
